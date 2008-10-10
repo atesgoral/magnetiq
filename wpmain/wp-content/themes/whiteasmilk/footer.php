42ac49efa4bebc25068e2796f900681f91f64329
atesgoral <ates@magnetiq.com>
2008-10-10 17:05:13 +0000

    Added Google Analytics code
    --HG--
    extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%4068

--- wpmain/wp-content/themes/whiteasmilk/footer.php
+++ wpmain/wp-content/themes/whiteasmilk/footer.php
@@ -15,5 +15,13 @@
 
 		<?php do_action('wp_footer'); ?>
 
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-912088-1");
+pageTracker._trackPageview();
+</script>
 </body>
 </html>
