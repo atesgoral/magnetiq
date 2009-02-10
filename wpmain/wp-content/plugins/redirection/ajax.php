12b20128469314bec905a66c249e2f1c465e7435
atesgoral <ates@magnetiq.com>
2009-02-10 03:53:50 +0000

    --HG-- extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40117

---
+++ wpmain/wp-content/plugins/redirection/ajax.php
@@ -0,0 +1,368 @@
+<?php
+/**
+ * Redirection
+ *
+ * @package Redirection
+ * @author John Godley
+ * @copyright Copyright (C) John Godley
+ **/
+
+/*
+============================================================================================================
+This software is provided "as is" and any express or implied warranties, including, but not limited to, the
+implied warranties of merchantibility and fitness for a particular purpose are disclaimed. In no event shall
+the copyright owner or contributors be liable for any direct, indirect, incidental, special, exemplary, or
+consequential damages (including, but not limited to, procurement of substitute goods or services; loss of
+use, data, or profits; or business interruption) however caused and on any theory of liability, whether in
+contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of
+this software, even if advised of the possibility of such damage.
+
+For full license details see license.txt
+============================================================================================================ */
+
+if (file_exists ('../../../wp-load.php'))
+	include ('../../../wp-load.php');
+else
+	include ('../../../wp-config.php');
+
+function urldecode_deep ($data)
+{
+	if (is_array ($data))
+	{
+		foreach ($data AS $key => $values)
+			$data[$key] = urldecode_deep ($values);
+			
+		return $data;
+	}
+	
+	return rawurldecode ($data);
+}
+
+class Redirection_AJAX extends Redirection_Plugin
+{
+	function Redirection_AJAX ($id, $command)
+	{
+		include (dirname (__FILE__).'/models/pager.php');
+		
+		if (!current_user_can ('edit_plugins'))
+			die (__ ('<p style="color: red">You are not allowed access to this resource</p>', 'redirection'));
+		
+		$_POST = stripslashes_deep ($_POST);
+		
+		$this->register_plugin ('redirection', __FILE__);
+		if (method_exists ($this, $command))
+			$this->$command ($id);
+		else
+			die (__ ('<p style="color: red">That function is not defined</p>', 'redirection'));
+	}
+	
+	function add_redirect ($id)
+	{
+		$_POST = urldecode_deep ($_POST);
+		
+		$item = Red_Item::create ($_POST);
+
+		if ($item !== false)
+		{
+			echo '<li class="type_'.$item->action_type.'" id="item_'.$item->id.'">';
+			$this->render_admin ('item', array ('redirect' => $item, 'date_format' => get_option ('date_format')));
+			echo '</li>';
+		}
+		else
+			$this->render_error (__ ('Sorry, but your redirection was not created', 'redirection'));
+	}
+	
+	function show_redirect ($id)
+	{
+		$redirect = Red_Item::get_by_id ($id);
+		if ($redirect)
+			$this->render_admin ('item_edit', array ('redirect' => $redirect, 'groups' => Red_Group::get_for_select ()));
+	}
+	
+	function save_redirect ($id)
+	{
+		$_POST = urldecode_deep ($_POST);
+		
+		$redirect = Red_Item::get_by_id ($id);
+		$redirect->update ($_POST);
+		
+		$this->render_admin ('item', array ('redirect' => $redirect, 'date_format' => get_option ('date_format')));
+	}
+	
+	function cancel_redirect ($id)
+	{
+		$redirect = Red_Item::get_by_id ($id);
+		if ($redirect)
+			$this->render_admin ('item', array ('redirect' => $redirect, 'date_format' => get_option ('date_format')));
+	}
+	
+	function toggle_redirects ($id)
+	{
+		$items = array_filter (explode ('-', $id));
+		if (count ($items) > 0)
+		{
+			foreach ($items AS $item)
+			{
+				$redirect = Red_Item::get_by_id ($item);
+				$redirect->toggle_status ();
+			}
+			
+			$group = Red_Group::get ($redirect->group_id);
+			Red_Module::flush ($group->module_id);
+		}
+	}
+	
+	function move_redirects ($id)
+	{
+		$group = intval ($_GET['group']);
+		$items = array_filter (explode ('-', $id));
+		if (count ($items) > 0)
+		{
+			foreach ($items AS $item)
+			{
+				$redirect = Red_Item::get_by_id ($item);
+				$redirect->move_to ($group);
+			}
+		}
+	}
+	
+	function reset_redirects ($id)
+	{
+		$group = intval ($_GET['group']);
+		$items = array_filter (explode ('-', $id));
+		if (count ($items) > 0)
+		{
+			foreach ($items AS $item)
+			{
+				$redirect = Red_Item::get_by_id ($item);
+				$redirect->reset ($group);
+			}
+		}
+	}
+
+	function save_redirect_order ($start)
+	{
+		Red_Item::save_order ($_POST['items'], $start);
+	}
+	
+	function reset_redirect ($id)
+	{
+		$redirect = Red_Item::get_by_id ($id);
+		if ($redirect)
+		{
+			$redirect->reset ();
+			$this->render_admin ('item', array ('redirect' => $redirect, 'date_format' => get_option ('date_format')));
+		}
+	}
+	
+	function show_log ($id)
+	{
+		$log      = RE_Log::get_by_id ($id);
+		$redirect = Red_Item::get_by_id ($log->redirection_id);
+		
+		$this->render_admin ('log_item_details', array ('log' => $log, 'redirect' => $redirect));
+	}
+	
+	function hide_log ($id)
+	{
+		$log = RE_Log::get_by_id ($id);
+		echo '<a href="#" onclick="return toggle_log('.$id.')">'.$log->show_url ($log->url).'</a>';
+	}
+	
+	function show_404 ($id)
+	{
+		$log      = RE_Log::get_by_id ($id);
+		$redirect = Red_Item::get_by_id ($log->redirection_id);
+		
+		$this->render_admin ('404_item_details', array ('log' => $log, 'redirect' => $redirect));
+	}
+	
+	function hide_404 ($id)
+	{
+		$log = RE_Log::get_by_id ($id);
+		echo '<a href="#" onclick="return toggle_404('.$id.')">'.$log->show_url ($log->url).'</a>';
+	}
+	
+	function delete_logs ($id)
+	{
+		$groups = array_filter (explode ('-', $_POST['ids']));
+		if (count ($groups) > 0)
+		{
+			foreach ($groups AS $group)
+				RE_Log::delete ($group);
+		}
+	}
+	
+	
+	
+	function delete_items ($id)
+	{
+		$groups = array_filter (explode ('-', $_POST['ids']));
+		if (count ($groups) > 0)
+		{
+			$item = Red_Item::get_by_id ($groups[0]);
+
+			foreach ($groups AS $group)
+				Red_Item::delete ($group);
+
+			$group = Red_Group::get ($item->group_id);
+			Red_Module::flush ($group->module_id);
+		}
+	}
+	
+	function delete_groups ($id)
+	{
+		$groups = array_filter (explode ('-', $_POST['ids']));
+		if (count ($groups) > 0)
+		{
+			foreach ($groups AS $group)
+				Red_Group::delete (intval ($group));
+		}
+		
+		$group = Red_Group::get ($group);
+		Red_Module::flush ($group->module_id);
+	}
+
+	function toggle_groups ($id)
+	{
+		$groups = array_filter (explode ('-', $id));
+		if (count ($groups) > 0)
+		{
+			foreach ($groups AS $group)
+			{
+				$group = Red_Group::get ($group);
+				$group->toggle_status ();
+			}
+			
+			Red_Module::flush ($group->module_id);
+		}
+	}
+
+	function edit_group ($id)
+	{
+		$group = Red_Group::get ($id);
+		if ($group)
+			$this->render_admin ('group_edit', array ('group' => $group, 'modules' => Red_Module::get_for_select ()));
+		else
+			$this->render_admin ('error', array ('message' => __ ('Failed to retrieve group data', 'redirection'), 'file' => __FILE__, 'line' => __LINE__));
+	}
+	
+	function cancel_group ($id)
+	{
+		$group = Red_Group::get ($id);
+		if ($group)
+			$this->render_admin ('group_item', array ('group' => $group));
+		else
+			$this->render_admin ('error', array ('message' => __ ('Failed to retrieve group data', 'redirection'), 'file' => __FILE__, 'line' => __LINE__));
+	}
+	
+	function save_group_order ($start)
+	{
+		Red_Group::save_order ($_POST['items'], $start);
+	}
+	
+	function save_group ($id)
+	{
+		$group = Red_Group::get ($id);
+		if ($group)
+		{
+			$original_module = $group->module_id;
+			$group->update ($_POST);
+		
+			$this->render_admin ('group_item', array ('group' => $group));
+			
+			if ($group->status == 'enabled')
+				echo '<script type="text/javascript" charset="utf-8">$("item_'.$id.'").removeClassName ("disabled");</script>';
+			else
+				echo '<script type="text/javascript" charset="utf-8">$("item_'.$id.'").addClassName ("disabled");</script>';
+		}
+		else
+			$this->render_admin ('error', array ('message' => __ ('Failed to retrieve group data', 'redirection'), 'file' => __FILE__, 'line' => __LINE__));
+	}
+	
+	function move_groups ($id)
+	{
+		$group = intval ($_GET['group']);
+		$items = array_filter (explode ('-', $id));
+		if (count ($items) > 0)
+		{
+			foreach ($items AS $item)
+			{
+				$redirect = Red_Group::get ($item);
+				$redirect->move_to ($group);
+			}
+		}
+	}
+	
+	function reset_groups ($id)
+	{
+		$group = intval ($_GET['group']);
+		$items = array_filter (explode ('-', $id));
+		if (count ($items) > 0)
+		{
+			foreach ($items AS $item)
+			{
+				$redirect = Red_Group::get ($item);
+				$redirect->reset ($group);
+			}
+		}
+	}
+	
+	
+	function edit_module ($id)
+	{
+		$module = Red_Module::get ($id);
+		if ($module)
+			$this->render_admin ('module_edit', array ('module' => $module));
+		else
+			$this->render_admin ('error', array ('message' => __ ('Failed to retrieve module data', 'redirection'), 'file' => __FILE__, 'line' => __LINE__));
+	}
+	
+	function cancel_module ($id)
+	{
+		$module = Red_Module::get ($id);
+		if ($module)
+			$this->render_admin ('module_item', array ('module' => $module));
+		else
+			$this->render_admin ('error', array ('message' => __ ('Failed to retrieve group data', 'redirection'), 'file' => __FILE__, 'line' => __LINE__));
+	}
+	
+	function save_module ($id)
+	{
+		$module = Red_Module::get ($id);
+		if ($module)
+		{
+			$module->update ($_POST);
+		
+			$this->render_admin ('module_item', array ('module' => $module));
+		}
+		else
+			$this->render_admin ('error', array ('message' => __ ('Failed to retrieve group data', 'redirection'), 'file' => __FILE__, 'line' => __LINE__));
+	}
+	
+	function delete_module ($id)
+	{
+		$module = Red_Module::get ($id);
+		$module->delete ();
+	}
+	
+	function reset_module ($id)
+	{
+		$module = Red_Module::get ($id);
+		if ($module)
+		{
+			$module->reset ();
+			$this->render_admin ('module_item', array ('module' => $module));
+		}
+		else
+			$this->render_admin ('error', array ('message' => __ ('Failed to retrieve group data', 'redirection'), 'file' => __FILE__, 'line' => __LINE__));
+
+	}
+}
+
+$id  = $_GET['id'];
+$cmd = $_GET['cmd'];
+
+$obj = new Redirection_AJAX ($id, $cmd);
+
+?>
---
+++ wpmain/wp-content/plugins/redirection/js/redirection.js
@@ -0,0 +1,505 @@
+function update_count ()
+{
+  if ($('items').childNodes.length >= 2)
+  {
+    if ($('items').childNodes.length >= 25)
+      Element.show ('pager');
+    Element.show ('save_order');
+    Element.show ('redirections_header');
+  }
+  else
+  {
+    Element.hide ('save_order');
+    Element.hide ('redirections_header');
+  }
+  
+  if ($('items').childNodes.length < 25)
+    Element.hide ('pager');
+}
+
+// XXX update count for what???
+
+function encode_data (params)
+{
+  params.each (function (pair)
+  {
+    if (typeof pair.value == 'object')
+      params[pair.key].each (function (otherpair) { params[pair.key][otherpair.key] = escape (otherpair.value);});
+    else
+      params[pair.key] = escape (pair.value);
+  });
+  
+  return params;
+}
+
+function save_redirect (item,form)
+{
+  var params = $H(Form.serialize (form, true));
+
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=save_redirect&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      parameters: encode_data (params),
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+    });
+}
+
+function cancel_redirect (item)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=cancel_redirect&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+    });
+}
+
+function show_redirect (item)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=show_redirect&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;},
+      onComplete: function(request) {  }
+    });
+}
+
+function delete_redirect (item)
+{
+  new Effect.Pulsate ('item_' + item, { duration: 1.5, pulses: 2});
+  
+  if (confirm ("Are you sure you want to delete the redirection?"))
+  {
+    new Ajax.Request (wp_base + 'ajax.php?cmd=delete_redirect&id=' + item,
+      {
+        asynchronous: true, evalScripts: true,
+        onSuccess: function(request) { Element.remove ('item_' + item);update_count ();},
+        onLoading: function(request) { Element.show ('loading')},
+        onComplete: function(request) { Element.hide ('loading'); }
+      });
+  }
+}
+
+function add_redirection (form,add_to_screen)
+{
+  var params = $H(Form.serialize (form, true));
+  
+  new Ajax.Request (wp_base + 'ajax.php?cmd=add_redirect&id=0',
+    {
+      asynchronous: true, evalScripts: true,
+      parameters: params.each (function (pair) { params[pair.key] = escape (pair.value)}),
+      onSuccess: function(request)
+      {
+        if (request.responseText.indexOf ('fade error') != -1)
+          $('error').innerHTML = request.responseText;
+        else
+        {
+          if (add_to_screen == true)
+            new Insertion.Bottom('items', request.responseText);
+
+          Element.hide ('error');
+          Element.show ('added');
+          Element.hide ('none')
+        }
+      },
+      onLoading: function(request) { Element.show ('loading')},
+      onComplete: function(request) { Element.hide ('loading'); update_count (); }
+    });
+}
+
+function sort_order ()
+{
+  Sortable.create ('items', { ghosting: true });
+  Element.hide ('toggle_sort_on');
+  Element.show ('toggle_sort_off');
+  new Effect.Pulsate ('sort');
+  
+  var elements = $('items').getElementsBySelector ('li');
+  elements.each(function(item) { $(item).addClassName ('sortable')});
+  return false;
+}
+
+function save_redirect_order (start)
+{
+  if (confirm ('Are you sure you want to save the current order?'))
+  {
+    new Ajax.Request (wp_base + 'ajax.php?cmd=save_redirect_order&id=' + start,
+      {
+        asynchronous: true, evalScripts: true,
+        parameters: Sortable.serialize ('items'),
+        onLoading: function(request) { Element.show ('loading')},
+        onComplete: function(request)
+        {
+          Element.hide ('loading');
+          Element.hide ('toggle_sort_off');
+          Element.show ('toggle_sort_on');
+          
+          Sortable.destroy ('items');
+
+          var elements = $('items').getElementsBySelector ('li');
+          elements.each(function(item) { $(item).removeClassName ('sortable')});
+          
+          new Effect.Pulsate ('sort');
+        }
+      });
+  }
+  
+  return false;
+}
+
+
+function add_log_item (item)
+{
+  Element.hide ('added');
+  Element.show ('add');
+  
+  $('old').value = $('href_' + item).href.gsub (/\w*:\/\/(.*?)\//,'/');
+  new Effect.Pulsate ('submit');
+  Element.scrollTo ('add');
+  return false;
+}
+
+function toggle_log (item)
+{
+  if ($('info_' + item).getElementsByTagName ('table').length == 0)
+  {
+    new Ajax.Updater ('info_' + item, wp_base + 'ajax.php?cmd=show_log&id=' + item,
+      {
+        asynchronous: true, evalScripts: true,
+        onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+      });
+  }
+  else
+  {
+    new Ajax.Updater ('info_' + item, wp_base + 'ajax.php?cmd=hide_log&id=' + item,
+      {
+        asynchronous: true, evalScripts: true,
+        onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+      });
+  }
+  
+  return false;
+}
+
+function toggle_404 (item)
+{
+  if ($('info_' + item).getElementsByTagName ('table').length == 0)
+  {
+    new Ajax.Updater ('info_' + item, wp_base + 'ajax.php?cmd=show_404&id=' + item,
+      {
+        asynchronous: true, evalScripts: true,
+        onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+      });
+  }
+  else
+  {
+    new Ajax.Updater ('info_' + item, wp_base + 'ajax.php?cmd=hide_404&id=' + item,
+      {
+        asynchronous: true, evalScripts: true,
+        onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+      });
+  }
+  
+  return false;
+}
+
+function add_new_url (item)
+{
+  new Insertion.After(item, '<br/><input style="width: 90%" type="text" name="new[]"/>');
+}
+
+
+
+
+function save_group_order (start)
+{
+  if (confirm ('Are you sure you want to save the current order?'))
+  {
+    new Ajax.Request (wp_base + 'ajax.php?cmd=save_group_order&id=' + start,
+      {
+        asynchronous: true, evalScripts: true,
+        parameters: Sortable.serialize ('items'),
+        onLoading: function(request) { Element.show ('loading')},
+        onComplete: function(request)
+        {
+          Element.hide ('loading');
+          Element.hide ('toggle_sort_off');
+          Element.show ('toggle_sort_on');
+          
+          Sortable.destroy ('items');
+
+          var elements = $('items').getElementsBySelector ('li');
+          elements.each(function(item) { $(item).removeClassName ('sortable')});
+          
+          new Effect.Pulsate ('sort');
+        }
+      });
+  }
+}
+
+function edit_group (item)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=edit_group&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+    });
+  return false;
+}
+
+function cancel_group (item)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=cancel_group&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+    });
+  return false;
+}
+
+function save_group (item,form)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=save_group&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      parameters: Form.serialize (form),
+      onLoading: function(request){ $('info_' + item).innerHTML = wp_progress;}
+    });
+  return false;
+}
+
+function select_all ()
+{
+  var elements = $('items').getElementsBySelector ('input');
+  var todelete = '';
+  
+  elements.each (function(item)
+  {
+    item.checked = wp_red_select;
+  });
+  
+  wp_red_select = !wp_red_select;
+  return false;
+}
+
+
+function reset_items (type)
+{
+  var tochange = '', url;
+  var elements = $('items').getElementsBySelector ('input');
+
+  elements.each (function(item)
+  {
+    if (item.checked)
+      tochange += item.value + '-';
+  });
+  
+  if (tochange != '')
+  {
+    if (confirm ('Are you sure you want to reset statistics for these items?'))
+    {
+      if (type == 'group')
+       url = wp_base + 'ajax.php?cmd=reset_groups&id=' + tochange;
+      else
+       url = wp_base + 'ajax.php?cmd=reset_redirects&id=' + tochange;
+
+      new Ajax.Request (url,
+      {
+        asynchronous: true, evalScripts: true,
+        onLoading: function(request) { Element.show ('loading')},
+        onComplete: function(request) { window.location.reload (); }
+      });
+    }
+  }
+  else
+    alert ('You have not selected any items');
+
+  return false;
+}
+
+function toggle_items (type)
+{
+  var tochange = '', url;
+  var elements = $('items').getElementsBySelector ('input');
+
+  elements.each (function(item)
+  {
+    if (item.checked)
+      tochange += item.value + '-';
+  });
+  
+  if (tochange != '')
+  {
+    if (type == 'group')
+     url = wp_base + 'ajax.php?cmd=toggle_groups&id=' + tochange;
+    else
+     url = wp_base + 'ajax.php?cmd=toggle_redirects&id=' + tochange;
+ 
+    new Ajax.Request (url,
+    {
+      asynchronous: true, evalScripts: true,
+      onLoading: function(request) { Element.show ('loading')},
+      onComplete: function(request) { window.location.reload (); }
+    });
+  }
+  else
+    alert ('You have not selected any items');
+
+  return false;
+}
+
+function move_items (type)
+{
+  var tochange = '', url;
+  var elements = $('items').getElementsBySelector ('input');
+
+  elements.each (function(item)
+  {
+    if (item.checked)
+      tochange += item.value + '-';
+  });
+  
+  if (tochange != '')
+  {
+    if (type == 'group')
+      url = wp_base + 'ajax.php?cmd=move_groups&id=' + tochange;
+    else
+      url = wp_base + 'ajax.php?cmd=move_redirects&id=' + tochange;
+    
+    url += '&group=' + $('move').value;
+    
+    new Ajax.Request (url,
+      {
+        asynchronous: true, evalScripts: true,
+        onSuccess: function(request)
+        {
+          elements.each (function(item)
+          {
+            if (item.checked)
+              Element.remove ('item_' + item.value);
+          });
+        },
+        onLoading: function(request) { Element.show ('loading')},
+        onComplete: function(request) { Element.hide ('loading'); }
+      });
+  }
+  else
+    alert ('You have not selected any items');
+  return false;
+}
+
+function delete_items (type)
+{
+  var tochange = '', url;
+  var elements = $('items').getElementsBySelector ('input');
+
+  elements.each (function(item)
+  {
+    if (item.checked)
+      tochange += item.value + '-';
+  });
+  
+  if (tochange != '')
+  {
+    if (confirm ('Are you sure this is what you want to do?'))
+    {
+      if (type == 'group')
+        url = wp_base + 'ajax.php?cmd=delete_groups&id=0';
+      else if (type == 'log')
+        url = wp_base + 'ajax.php?cmd=delete_logs&id=0';
+      else
+        url = wp_base + 'ajax.php?cmd=delete_items&id=0';
+      
+      new Ajax.Request (url,
+        {
+          asynchronous: true,
+          evalScripts: true,
+          parameters: 'ids=' + tochange,
+          onSuccess: function(request)
+          {
+            elements.each (function(item)
+            {
+              if (item.value && item.checked)
+                Element.remove ('item_' + item.value);
+            });
+          },
+          onLoading: function(request) { Element.show ('loading')},
+          onComplete: function(request) { Element.hide ('loading'); }
+        });
+    }
+  }
+  else
+    alert ('You have not selected any items');
+  return false;
+}
+
+function delete_module (item)
+{
+  if (confirm ("Are you sure you want to delete this?"))
+  {
+    new Ajax.Request (wp_base + 'ajax.php?cmd=delete_module&id=' + item,
+      {
+        asynchronous: true, evalScripts: true,
+        onSuccess: function(request) { Element.remove ('item_' + item);},
+        onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+      });
+  }
+  return false;
+}
+
+function reset_module (item)
+{
+  if (confirm ("Are you sure you want to reset this?"))
+  {
+    new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=reset_module&id=' + item,
+      {
+        asynchronous: true, evalScripts: true,
+        onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+      });
+  }
+  return false;
+}
+
+function edit_module (item)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=edit_module&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+    });
+  return false;
+}
+
+function save_module (item,form)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=save_module&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      parameters: Form.serialize (form),
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+    });
+  return false;
+}
+
+function cancel_module (item)
+{
+  new Ajax.Updater ('item_' + item, wp_base + 'ajax.php?cmd=cancel_module&id=' + item,
+    {
+      asynchronous: true, evalScripts: true,
+      onLoading: function(request) { $('info_' + item).innerHTML = wp_progress;}
+    });
+  return false;
+}
+
+function update_user_agent (item,box)
+{
+  $('user_agent_' + box).value = $(item).value;
+}
+
+
+function change_add_redirect (item)
+{
+  if (item.value == 'url' || item.value == 'pass')
+    Element.show ('target');
+  else
+    Element.hide ('target');
+}
