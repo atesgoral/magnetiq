5aedd72d1c313ea3fade39325c6e44d816a91f0b
atesgoral <ates@magnetiq.com>
2009-05-09 05:05:03 +0000

    --HG-- rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/404.php => magnetiq.com/theme/404.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/archive.php => magnetiq.com/theme/archive.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/archives.php => magnetiq.com/theme/archives.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/comments-popup.php => magnetiq.com/theme/comments-popup.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/comments.php => magnetiq.com/theme/comments.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/footer.php => magnetiq.com/theme/footer.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/header.php => magnetiq.com/theme/header.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/index.php => magnetiq.com/theme/index.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/links.php => magnetiq.com/theme/links.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/page.php => magnetiq.com/theme/page.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/search.php => magnetiq.com/theme/search.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/searchform.php => magnetiq.com/theme/searchform.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/sidebar.php => magnetiq.com/theme/sidebar.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/single.php => magnetiq.com/theme/single.php rename : magnetiq.com/wpmain/wp-content/themes/whiteasmilk/style.css => magnetiq.com/theme/style.css extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40236

---
+++ theme/single.php
@@ -0,0 +1,90 @@
+
+<?php get_header(); ?>
+
+		<?php get_sidebar(); ?>
+
+	<div id="content" class="narrowcolumn" style="margin:0px; ">
+
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	
+		<div class="navigation">
+			<div class="alignleft"><?php previous_post('&laquo; %','','yes') ?></div>
+			<div class="alignright"><?php next_post(' % &raquo;','','yes') ?></div>
+		</div>
+
+		<div class="post">
+
+<?php
+$digg_url = get_post_custom_values("digg_url");
+if (!empty($digg_url)):
+?>
+			<script type="text/javascript">
+			digg_url = "<?php echo $digg_url[0]; ?>";
+			</script>
+			<div id="digg">
+			<script src="http://digg.com/api/diggthis.js"></script>
+			</div>
+<?php endif ?>
+			
+			<h2 id="post-<?php the_ID(); ?>"><a href="<?php echo get_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+			<small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
+			<!--p class="ad">
+<script type="text/javascript">
+google_ad_client = "pub-2115594125871453";
+/* post 468x15 */
+google_ad_slot = "5193778318";
+google_ad_width = 468;
+google_ad_height = 15;
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+			</p-->
+
+			<div class="entry">			
+				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
+	
+				<?php link_pages('<p><strong>Pages:</strong> ', '</p>', 'number'); ?>
+				
+			<!--p class="ad">
+<script type="text/javascript">
+google_ad_client = "pub-2115594125871453";
+/* post 468x15 */
+google_ad_slot = "5193778318";
+google_ad_width = 468;
+google_ad_height = 15;
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+			</p-->
+
+				<p class="postmetadata alt">
+					<small>
+						This entry was posted
+						<?php /* This is commented, because it requires a little adjusting sometimes.
+							You'll need to download this plugin, and follow the instructions:
+							http://binarybonsai.com/archives/2004/08/17/time-since-plugin/ */
+							/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?> 
+						on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
+						and is filed under <?php the_category(', ') ?>.
+						 
+						<?php  edit_post_link('Edit this entry.','',''); ?>
+						
+					</small>
+				</p>
+	
+			</div>
+		</div>
+		
+	<?php comments_template(); ?>
+	
+	<?php endwhile; else: ?>
+	
+		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+	
+<?php endif; ?>
+
+	</div>
+
+<?php get_footer(); ?>
