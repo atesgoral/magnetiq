cca51ce126499e2bdad3a81dc4c5cd58a96e91a7
atesgoral <ates@magnetiq.com>
2010-01-04 03:50:03 +0000

    OMG. (mt) -> WebFaction
    --HG--
    extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40318

--- theme/footer.php
+++ theme/footer.php
@@ -2,7 +2,7 @@
 <div id="footer">
 &copy; 1998-2010 Ates Goral and licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/2.0/">Creative Commons License</a>.
 <br />
-<a href="http://www.mediatemple.net/go/order/?refdom=magnetiq.com">(mt) Media Temple</a> |
+<a href="http://www.webfaction.com/?affiliate=atesgoral">WebFaction</a> |
 <a href="http://wordpress.org">WordPress</a> |
 <a href="http://feeds.feedburner.com/magnetiq_rss">Entries (RSS)</a> |
 <a href="http://feeds.feedburner.com/magnetiq_atom">Entries (Atom)</a> |
