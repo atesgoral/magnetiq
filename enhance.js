4e15b2b9a5cc9669639340efaf9015e85924e630
atesgoral <ates@magnetiq.com>
2010-03-26 07:13:44 +0000

    --HG-- extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40372

--- enhance.js
+++ enhance.js
@@ -1,37 +1,8 @@
 $(document).ready(function () {
 	$("#projects a").each(function (i, a) {
 		(function (idx, project) {
-			//console.log(project);
-
-			a.bg =
 			a.style.backgroundImage =
 				"url(project_images/" + project + ".jpg)";
-			
-			$(a).hover(function () {
-				var start = idx - idx % 3;
-				
-				console.log(project);
-				
-				$("#projects a").slice(start, start + 6).each(function (i, a) {
-					//console.log(i);
-					a.style.backgroundImage =
-						"url(project_images/l_" + project + ".jpg)";
-					var x = i % 3;
-					var y = Math.floor(i / 3);
-					console.log([x, y]);
-					a.style.backgroundPosition = (-x * 240) + "px " + (-y * 296) + "px";
-				});
-			}, function () {
-				var start = idx - idx % 3;
-				
-				console.log(project);
-				
-				$("#projects a").slice(start, start + 6).each(function (i, a) {
-					//console.log(i);
-					a.style.backgroundImage = a.bg;
-					a.style.backgroundPosition = "0 0";
-				});
-			});
 		})(i, /#(.+)/.exec(a.href)[1]);
 	});
 });
