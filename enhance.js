6032cd35f6eb7afdd1fac2f7e3376da741751f34
atesgoral <ates@magnetiq.com>
2010-03-27 19:38:51 +0000

    --HG-- extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40374

--- enhance.js
+++ enhance.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
-	$("#panels a").each(function (i, a) {
+	// Add panel backgrounds
+	$(".panels a").each(function (i, a) {
 		(function (idx, panel) {
 			a.style.backgroundImage =
 				"url(project_images/" + panel + ".jpg)";
 		})(i, /#(.+)/.exec(a.href)[1]);
 	});
+
+	// Clone navigation
+	var nav = $(".nav");
+	$(".section_end").after(nav.clone());
 });
