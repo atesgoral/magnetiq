7197878ec5e693a77ab0ca612743687ca165d146
atesgoral <ates@magnetiq.com>
2010-04-13 17:54:23 +0000

    --HG-- rename : magnetiq.com/favicon.ico => magnetiq.com/.attic/favicon.ico rename : magnetiq.com/fluid.html => magnetiq.com/.attic/fluid.html rename : magnetiq.com/page.html => magnetiq.com/.templates/page.html rename : magnetiq.com/js/Museo_700_600.font.js => magnetiq.com/lib/cufon/Museo_700_600.font.js rename : magnetiq.com/js/cufon-yui.js => magnetiq.com/lib/cufon/cufon-yui.js extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40452

--- compile.py
+++ compile.py
@@ -1,28 +1,35 @@
 import os
 import re
 
-f = open("page.html", "r")
+template_dir = ".templates"
+content_dir = ".content"
+output_dir = "pages"
+
+f = open(os.path.join(template_dir, "page.html"), "r")
 template = f.read();
 f.close()
 
-for page in os.listdir(".content"):
-	# Load content
-	f = open(".content/" + page, "r")
-	content_html = f.read()
-	f.close()
+for page in os.listdir(content_dir):
+	content_file = os.path.join(content_dir, page)
 	
-	# Extract info
-	info = re.match("<h2>(?P<title>.+)</h2>", content_html)
-	
-	# Add permalink
-	content_html = re.sub("(<h2>.+</h2>)",
-		r'<a href="#" title="Permanent Link">\1</a>', content_html)
-	
-	# Insert info
-	page_html = template.format(
-		Title = info.group("title"),
-		Content = content_html)
-	
-	f = open("pages/" + page, "w")
-	f.write(page_html)
-	f.close
+	if os.path.isfile(content_file):
+		# Load content
+		f = open(content_file, "r")
+		content_html = f.read()
+		f.close()
+		
+		# Extract info
+		info = re.match("<h2>(?P<title>.+)</h2>", content_html)
+		
+		# Add permalink
+		content_html = re.sub("(<h2>.+</h2>)",
+			r'<a href="#" title="Permanent Link">\1</a>', content_html)
+		
+		# Insert info
+		page_html = template.format(
+			Title = info.group("title"),
+			Content = content_html)
+		
+		f = open(os.path.join(output_dir, page), "w")
+		f.write(page_html)
+		f.close
