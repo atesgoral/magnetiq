144f6435352fe1c6ccc1ce96cb819ec812bfaaff
atesgoral <ates@magnetiq.com>
2010-04-15 18:22:25 +0000

    --HG-- extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40500

--- js/enhance_tumblr.js
+++ js/enhance_tumblr.js
@@ -1,17 +1,7 @@
-var meta = {};
-
-$("meta").each(function () {
-    var tokens = /(.+):(.+)/.exec(this.name);
-
-    if (tokens) {
-        meta[tokens[2]] = this.content;
-    }
-});
-
-if (meta["Font Replacement"]) {
+if (meta.FontReplacement) {
     Cufon.replace("h1, h2");
 }
 
-if (meta["Permalink Page"] && meta["Syntax Highlighting"]) {
+if (meta.PermalinkPage && meta.SyntaxHighlighting) {
     SyntaxHighlighter.all();
 }
