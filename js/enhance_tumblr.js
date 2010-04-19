64e1c4a1c9278cefa39c4f5a1cc92ca478bd61c2
atesgoral <ates@magnetiq.com>
2010-04-19 21:09:41 +0000

    --HG-- extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40575

--- js/enhance_tumblr.js
+++ js/enhance_tumblr.js
@@ -5,3 +5,9 @@ if (meta.FontReplacement) {
 if (meta.PermalinkPage && meta.SyntaxHighlighting) {
     SyntaxHighlighter.all();
 }
+
+if (meta.PermalinkPage && meta.DisqusShortname) {
+    $.getScript("http://disqus.com/forums/" + meta.DisqusShortname
+        + "/get_num_replies.js?url0="
+        + encodeURIComponent($("#comment_count").attr("href")));
+}
