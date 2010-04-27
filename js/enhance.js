969fc67d0c0b9aa793044340cd8afecc445fb9b9
atesgoral <ates@magnetiq.com>
2010-04-27 06:17:05 +0000

    --HG-- extra : convert_revision : svn%3A9c75bc79-340e-b94c-9ff1-bf8a133d7eec/%40619

--- js/enhance.js
+++ js/enhance.js
@@ -1,7 +1,38 @@
 Cufon.replace("h2");
-var disqus_developer = 1;
+//var disqus_developer = 1;
 
 $(function () {
+	// Add panel backgrounds
+	$(".panels li").each(function () {
+		var a = $("a", this).get(0);
+		var tokens = /p_(.+)/.exec(a.className);
+		
+		if (tokens) {
+			a.style.backgroundImage =
+				"url(http://s.magnetiq.com/panel_thumbs/" + tokens[1] + ".png)";
+			$(this).addClass("panel");
+			$(a).addClass("panel");
+		}
+	});
+
+	if ($("body").hasClass("index")) {
+		// Clone navigation and add top borders
+		var nav = $("#about");
+		
+		$(".section").slice(1).addClass("top_border").each(function () {
+			var h2 = $("h2", this);
+			
+			h2.before(
+				nav.clone().attr("id", h2.attr("id"))
+			).attr("id", "");
+		});
+
+		// Workaround for target anchor not being in right place
+		setTimeout(function () {
+			with (document.location) hash && (hash = hash);
+		}, 1);
+	}
+
     // Add grid toggler
     $("#toggle_grid").click(function () {
         $("body").toggleClass("show_grid");
